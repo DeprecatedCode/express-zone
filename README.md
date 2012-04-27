2a2f939d4ec397517ee78da7bd9f2b0aa6512007
Nate Ferrero <nateferrero@gmail.com>
2012-04-27 04:05:20 +0000

    Functional

--- README.md
+++ README.md
@@ -2,3 +2,44 @@ express-zone
 ============
 
 Zones for Express
+
+Zones replaces the normal routing area of your Express application to make it more compartmentalized. In essense, all it does is virtualize all routing after the specified zone path. To use, just replace the normal routing section in your app.js (app.get etc) with the following:
+
+	/**
+	 * Use Express Zones
+	 */
+	var zone = require('express-zone').init(app, __dirname + '/zones');
+	zone("/hello-there", "hello");
+	zone("", "main");
+
+The init method takes two arguments, the first is the Express app, and the second is the folder housing the zones. Each zone is a folder containing a zone.js file, for example ./zones/hello/zone.js:
+
+	/**
+	 * Sample Zone
+	 * @author Nate Ferrero
+	 */
+	module.exports = function(zone) {
+
+		/**
+		 * Do Normal Express Routing, but on the zone.
+		zone.get('', function(req, res) {
+			res.send("Hello to you too!");
+		});
+
+		zone.get('/test', function(req, res) {
+			res.send("Hello test!");
+		});
+
+		zone.get('/test/apple', function(req, res) {
+			res.send("Hello apple!");
+		});
+
+	}
+
+Then, run your app and access the following routes:
+
+	/hello-there
+	/hello-there/test
+	/hello-there/test/apple
+
+And observe great success! Of course, this is compatible with any Express-recognized path format, such as including :id in the URL.
---
+++ index.js
@@ -0,0 +1,85 @@
+/**
+ * Express Zone
+ * Virtualizes all routing below specified path
+ * @author Nate Ferrero
+ */
+module.exports.init = function(app, root) {
+
+	/**
+	 * Zone Generator Function
+	 * @author Nate Ferrero
+	 */
+	return function(path, zone) {
+
+		/**
+		 * Strict Syntax
+		 * @author Nate Ferrero
+		 */
+		if(path.length && path[path.length - 1] == '/')
+			throw new Error("Zone path must not end in /");
+
+		if(path.length && path[0] != '/')
+			throw new Error("Zone path if not empty must begin with /");
+
+		/**
+		 * Fix Subpath
+		 * @author Nate Ferrero
+		 */
+		function fixPath(subpath) {
+
+			/**
+			 * Strict Syntax
+			 * @author Nate Ferrero
+			 */
+			if(subpath.length && subpath[0] != '/')
+				throw new Error("Zone subpath if not empty must begin with /");
+
+			return path + subpath;
+		}
+
+		/**
+		 * Expose Express Methods
+		 * @author Nate Ferrero
+		 */
+		var expressWrapper = {
+
+			/**
+			 * Get Method
+			 * @author Nate Ferrero
+			 */
+			get: function(subpath, callback) {
+				app.get(fixPath(subpath), callback);
+			},
+
+			/**
+			 * Post Method
+			 * @author Nate Ferrero
+			 */
+			post: function(subpath, callback) {
+				app.post(fixPath(subpath), callback);
+			},
+
+			/**
+			 * Put Method
+			 * @author Nate Ferrero
+			 */
+			put: function(subpath, callback) {
+				app.put(fixPath(subpath), callback);
+			},
+
+			/**
+			 * Delete Method
+			 * @author Nate Ferrero
+			 */
+			del: function(subpath, callback) {
+				app.del(fixPath(subpath), callback);
+			},
+		};
+
+		/**
+		 * Include the Zone
+		 * @author Nate Ferrero
+		 */
+		require(root+'/'+zone+'/zone.js')(expressWrapper);
+	}
+}
